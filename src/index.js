fd5bfe6a0edc84b96e254624361dc3e0ae518116
IharMrachko <39693287+IharMrachko@users.noreply.github.com>
2020-07-29 18:03:01 +0300

    finish functional (#3)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import './module'
+import {Excel} from "@/components/excel/Excel";
 import './scss/index.scss'
+import {Header} from "@/components/header/Header";
+import {Toolbar} from "@/components/toolbar/Toolbar";
+import {Formula} from "@/components/formula/Formula";
+import {Table} from "@/components/table/Table";
+
+const excel = new Excel('#app', {
+    components: [Header, Toolbar, Formula, Table]
+});
+
+excel.render();
+
+
+// const el = document.querySelector('#app');
+// const node = document.createElement('h1');
+// node.textContent = 'Inner node'
+// el.append(node);
