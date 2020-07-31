f0987cb895384f57aec90faab686d5ef76d2394d
IharMrachko <39693287+IharMrachko@users.noreply.github.com>
2020-07-31 15:08:48 +0300

    Finish Table Resize (#4)

---
+++ src/components/table/table.resize.js
@@ -0,0 +1,52 @@
+import {$} from "@core/dom";
+
+export function tableResizeHandler($root, event) {
+    const $resizer = $(event.target);
+    const $parent = $resizer.closest('[data-type="resizable"]');
+    const coords = $parent.getCoords();
+    const type = $resizer.data.resize;
+    const sideProp = type === 'col' ? 'bottom' : 'right';
+    $resizer.css({
+        opacity: 1,
+        zIndex: 1000,
+        [sideProp]: '-5000px'
+    });
+    const cells = $root.findAll(`[data-col="${$parent.data.col}"]`);
+    let value;
+
+    document.onmousemove = e => {
+        if(type === 'col'){
+            const delta = e.pageX - coords.right;
+            value = coords.width + delta;
+            $resizer.css({right: -delta + 'px'});
+        } else {
+            const delta = e.pageY - coords.bottom;
+            value = coords.height + delta;
+            $resizer.css({bottom: -delta + 'px'});
+        }
+    };
+
+    document.onmouseup = () => {
+        document.onmousemove = null;
+        document.onmouseup = null;
+        if (type === 'col') {
+            $parent.css({width: value + 'px'});
+            $root.findAll(`[data-col="${$parent.data.col}"]`);
+        } else {
+            $parent.css({
+                height: value + 'px'
+            });
+        }
+
+        $resizer.css({
+            opacity: 0,
+            bottom: 0,
+            right: 0
+        });
+        $parent.css({
+            width: value + 'px'
+        });
+        cells
+            .forEach(el => el.style.width = value + 'px')
+    }
+}
